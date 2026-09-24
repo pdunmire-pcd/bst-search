e0ed52188c2d2166fe8a476a6f9a3a599dc4f0f8
pdunmire-pcd <203000868+pdunmire-pcd@users.noreply.github.com>
2026-09-23 19:43:48 -0700

    Add test: contains_targetNotInTree_returnsFalse

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -38,5 +38,17 @@ public class BstSearchTest {
     boolean result = BstSearch.contains(root, 15);
     assertTrue(result);
    }
+
+   @Test
+    public void contains_targetNotInTree_returnsFalse() {
+        BinaryTreeNode<Integer> three = new BinaryTreeNode<>(3);
+        BinaryTreeNode<Integer> seven = new BinaryTreeNode<>(7);
+        BinaryTreeNode<Integer> fifteen = new BinaryTreeNode<>(15);
+        BinaryTreeNode<Integer> five = new BinaryTreeNode<>(5, three, seven);
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10, five, fifteen);
+        boolean result = BstSearch.contains(root, 6);
+        assertFalse(result);
+}
+
    
 }
