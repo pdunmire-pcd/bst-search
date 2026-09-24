8d941a5971961bfac4f68383019ce9b80f88a9de
pdunmire-pcd <203000868+pdunmire-pcd@users.noreply.github.com>
2026-09-23 18:28:04 -0700

    Add test: contains_emptyTree_returnsFalse

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,5 +3,10 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
-   
+   @Test
+   public void contains_emptyTree_returnsFalse() {
+    BinaryTreeNode<Integer> root = null;
+    boolean result = BstSearch.contains(root, 5);
+    assertFalse(result);
+   }
 }
